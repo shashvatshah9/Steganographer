31ef1224f123be36a97113fac557976714f256b9
Lovlin Thakkar <lovlinthakkar99@gmail.com>
2019-10-26 17:40:46 +0530

    update Hashing.java

--- src/stegnography/Hashing.java
+++ src/stegnography/Hashing.java
@@ -12,26 +12,28 @@ import java.security.MessageDigest;
 import java.util.Arrays;
 
 /**
- * 
+ *
  * @author SSHAH
+ * @author lovlin-thakkar
  */
 public class Hashing {
+    
     private byte[] enc;
     MessageDigest md;
+    
     /**
      * It returns the hashed value of the argument password passed by the user.
      * 
      * @param password to unlock the given document
      * @return the encoded value of the password
      */ 
-    public Hashing() throws NoSuchAlgorithmException{
-        enc=null;
+    public Hashing() throws NoSuchAlgorithmException {
+        enc = null;
         md = MessageDigest.getInstance("SHA-256");
         md.reset();
     }
     
-    public byte[] genHash(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException{
-    
+    public byte[] genHash(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         enc = password.getBytes("UTF-8");
         return md.digest(enc);    
     } 
